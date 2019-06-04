addbafb7c748585ae63b6314a8f937bf64724a76
Stanislav <civictyper13@gmail.com>
2019-06-04 17:29:40 +0300

    Update Session Focus Page + View Session and Log Individuals page

--- src/pages/SessionFocus.vue
+++ src/pages/SessionFocus.vue
@@ -3,173 +3,159 @@
     <v-layout wrap>
 
       <v-toolbar color="secondary">
-        <v-btn icon flat :to="{ name: 'Session' }" class="mb-2">
+        <v-btn icon flat :to="{ name: 'LogIndividuals' }" class="mb-2">
           <v-icon>navigate_before</v-icon>
         </v-btn>
         <v-toolbar-title class="ml-1">Session Focus</v-toolbar-title>
       </v-toolbar>
 
-      <v-flex xs4 class="pt-3 text-xs-center">
-        <v-badge overlap color="secondary">
-          <template v-slot:badge>
+      <v-flex xs12 class="pa-2"></v-flex>
+
+      <v-flex xs4 class="pt-3 text-xs-center" v-for="item in skills" v-bind:key="item.name">
+        <v-badge overlap color="secondary" >
+          <template v-slot:badge v-if="item.tags">
             <v-icon>local_offer</v-icon>
           </template>
           <v-avatar
+            v-if="item.tags"
             color="primary"
+            @click="showDialogOfTags('consistTags')"
           >
             <v-icon dark>offline_bolt</v-icon>
           </v-avatar>
+          <v-avatar
+            v-else
+            color="secondary"
+            @click="showDialogOfTags"
+          >
+            <v-icon dark>offline_bolt</v-icon>
+          </v-avatar>
+
         </v-badge>
-        <p class="white--text">Skill name</p>
+        <p class="white--text">{{ item.name }}</p>
       </v-flex>
 
-      <v-flex xs4 class="pt-3 text-xs-center">
-        <v-badge overlap color="secondary">
+      <v-flex xs4 class="pt-3 text-xs-center" >
+        <v-badge overlap color="secondary" >
           <v-avatar
             color="secondary"
+            @click="showDialogOfTags"
           >
-            <v-icon dark>offline_bolt</v-icon>
+            <v-icon dark>add_circle</v-icon>
           </v-avatar>
-        </v-badge>
-        <p class="white--text">Skill name</p>
-      </v-flex>
 
-      <v-flex xs4 class="pt-3 text-xs-center">
-        <v-badge overlap color="secondary">
-          <v-avatar
-            color="secondary"
-          >
-            <v-icon dark>offline_bolt</v-icon>
-          </v-avatar>
         </v-badge>
-        <p class="white--text">Skill name</p>
-      </v-flex>
-
-      <v-flex xs4 class="pt-3 text-xs-center">
-        <v-badge overlap color="secondary">
-          <v-avatar
-            color="secondary"
-          >
-            <v-icon dark>offline_bolt</v-icon>
-          </v-avatar>
-        </v-badge>
-        <p class="white--text">Skill name</p>
-      </v-flex>
-
-      <v-flex xs4 class="pt-3 text-xs-center">
-        <v-badge overlap color="secondary">
-          <template v-slot:badge>
-            <v-icon>local_offer</v-icon>
-          </template>
-          <v-avatar
-            color="primary"
-          >
-            <v-icon dark>offline_bolt</v-icon>
-          </v-avatar>
-        </v-badge>
-        <p class="white--text">Skill name</p>
-      </v-flex>
-
-      <v-flex xs4 class="pt-3 text-xs-center">
-        <v-badge overlap color="secondary">
-          <v-avatar
-            color="secondary"
-          >
-            <v-icon dark>offline_bolt</v-icon>
-          </v-avatar>
-        </v-badge>
-        <p class="white--text">Skill name</p>
-      </v-flex>
-
-      <v-flex xs4 class="pt-3 text-xs-center">
-        <v-badge overlap color="secondary">
-          <v-avatar
-            color="secondary"
-          >
-            <v-icon dark>offline_bolt</v-icon>
-          </v-avatar>
-        </v-badge>
-        <p class="white--text">Skill name</p>
-      </v-flex>
-
-      <v-flex xs4 class="pt-3 text-xs-center">
-        <v-badge overlap color="secondary">
-          <v-avatar
-            color="secondary"
-          >
-            <v-icon dark>offline_bolt</v-icon>
-          </v-avatar>
-        </v-badge>
-        <p class="white--text">Skill name</p>
-      </v-flex>
-
-      <v-flex xs4 class="pt-3 text-xs-center">
-        <v-badge overlap color="secondary">
-          <v-avatar
-            color="secondary"
-          >
-            <v-icon dark>offline_bolt</v-icon>
-          </v-avatar>
-        </v-badge>
-        <p class="white--text">Skill name</p>
-      </v-flex>
-
-      <v-flex xs4 class="pt-3 text-xs-center">
-        <v-badge overlap color="secondary">
-          <v-avatar
-            color="secondary"
-          >
-            <v-icon dark>offline_bolt</v-icon>
-          </v-avatar>
-        </v-badge>
-        <p class="white--text">Skill name</p>
-      </v-flex>
-
-      <v-flex xs4 class="pt-3 text-xs-center">
-        <v-badge overlap color="secondary">
-          <v-avatar
-            color="secondary"
-          >
-            <v-icon dark>offline_bolt</v-icon>
-          </v-avatar>
-        </v-badge>
-        <p class="white--text">Skill name</p>
-      </v-flex>
-
-      <v-flex xs4 class="pt-3 text-xs-center">
-        <v-badge overlap color="secondary">
-          <v-avatar
-            color="secondary"
-          >
-            <v-icon dark>offline_bolt</v-icon>
-          </v-avatar>
-        </v-badge>
-        <p class="white--text">Skill name</p>
+        <p class="white--text">Add Skill</p>
       </v-flex>
 
       <v-flex xs8 offset-xs2 class="px-3 pt-3">
-        <v-btn color="primary" block large class="font-weight-bold">Finish</v-btn>
+        <v-btn color="primary" block large class="font-weight-bold" @click="dialogSaveSession = true">Finish</v-btn>
       </v-flex>
 
+      <v-flex xs12 class="pa-2"></v-flex>
 
-
-      <v-flex xs12>
-        <div style="height: 90px;"></div>
-
-        <v-fab-transition>
-          <v-btn
-            color="deep-orange lighten-1"
-            dark
-            fixed
-            bottom
-            right
-            fab
-            :to="{ name: 'Sessions' }"
+      <v-dialog
+        v-model="dialogAddTag"
+        width="500"
+      >
+        <v-card>
+          <v-card-title
+            class="headline"
           >
-            <v-icon class="d-flex">navigate_next</v-icon>
-          </v-btn>
-        </v-fab-transition>
-      </v-flex>
+            Tags
+          </v-card-title>
+
+          <v-card-text>
+            <v-layout row>
+              <v-flex xs9>
+                <v-text-field placeholder="Create a new tag..."></v-text-field>
+              </v-flex>
+              <v-flex xs3>
+                <v-btn color="primary darken-1" @click="dialogAddTag = false" block>+Add</v-btn>
+              </v-flex>
+            </v-layout>
+
+            <v-layout row>
+              <v-flex xs6>
+                <v-chip label color="secondary" text-color="white" class="d-block pa-2 ml-0">
+                  <v-icon left>label</v-icon>Frequent Tag 1
+                </v-chip>
+                <v-chip label color="secondary" text-color="white" class="d-block pa-2 ml-0">
+                  <v-icon left>label</v-icon>Frequent Tag 3
+                </v-chip>
+                <v-chip label color="secondary" text-color="white" class="d-block pa-2 ml-0">
+                  <v-icon left>label</v-icon>Frequent Tag 5
+                </v-chip>
+                <v-chip label v-if="consistTags" color="secondary darken-2" text-color="white" class="d-block pa-2 ml-0">
+                  <v-icon left>label</v-icon>Frequent Tag 7
+                </v-chip>
+                <v-chip label v-else color="secondary" text-color="white" class="d-block pa-2 ml-0">
+                  <v-icon left>label</v-icon>Frequent Tag 7
+                </v-chip>
+              </v-flex>
+              <v-flex xs6>
+                <v-chip label color="secondary" text-color="white" class="d-block pa-2 mr-0">
+                  <v-icon left>label</v-icon>Frequent Tag 2
+                </v-chip>
+
+                <v-chip label v-if="consistTags" color="secondary darken-2" text-color="white" class="d-block pa-2 mr-0">
+                  <v-icon left>label</v-icon>Frequent Tag 4
+                </v-chip>
+                <v-chip label v-else color="secondary" text-color="white" class="d-block pa-2 mr-0">
+                  <v-icon left>label</v-icon>Frequent Tag 4
+                </v-chip>
+
+                <v-chip label color="secondary" text-color="white" class="d-block pa-2 mr-0">
+                  <v-icon left>label</v-icon>Frequent Tag 6
+                </v-chip>
+                <v-chip label color="secondary" text-color="white" class="d-block pa-2 mr-0">
+                  <v-icon left>label</v-icon>Frequent Tag 8
+                </v-chip>
+              </v-flex>
+            </v-layout>
+
+          </v-card-text>
+
+          <v-divider></v-divider>
+
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="primary" flat @click="dialogAddTag = false; consistTags = false">Close</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+
+
+
+      <v-dialog
+        v-model="dialogSaveSession"
+        width="500"
+      >
+        <v-card>
+          <v-card-title
+            class="headline"
+          >
+            Save session Log?
+          </v-card-title>
+
+          <v-card-text>
+
+            <v-layout row>
+              <v-flex xs12>
+                You can edit or add comments from the home screen
+              </v-flex>
+            </v-layout>
+
+          </v-card-text>
+
+          <v-divider></v-divider>
+
+          <v-card-actions>
+            <v-btn color="primary darken-1" :to="{ name: 'Sessions' }" block>Yes</v-btn>
+            <v-btn color="primary lighten-1" @click="dialogSaveSession = false" block>No</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
 
     </v-layout>
   </v-container>
@@ -180,7 +166,91 @@
       name: 'SessionFocus',
       data () {
         return {
-          apple: 'asdasdasd'
+          dialogSaveSession: false,
+          dialogAddTag: false,
+          consistTags: false,
+          skills: [
+            {
+              name: 'Skill name 1',
+              avatar: 'offline_bolt',
+              tags: true
+            },
+            {
+              name: 'Skill name 2',
+              avatar: 'offline_bolt',
+              tags: false
+            },
+            {
+              name: 'Skill name 3',
+              avatar: 'offline_bolt',
+              tags: false
+            },
+            {
+              name: 'Skill name 4',
+              avatar: 'offline_bolt',
+              tags: false
+            },
+            {
+              name: 'Skill name 5',
+              avatar: 'offline_bolt',
+              tags: false
+            },
+            {
+              name: 'Skill name 6',
+              avatar: 'offline_bolt',
+              tags: false
+            },
+            {
+              name: 'Skill name 7',
+              avatar: 'offline_bolt',
+              tags: false
+            },
+            {
+              name: 'Skill name 8',
+              avatar: 'offline_bolt',
+              tags: true
+            },
+            {
+              name: 'Skill name 9',
+              avatar: 'offline_bolt',
+              tags: false
+            },
+            {
+              name: 'Skill name 10',
+              avatar: 'offline_bolt',
+              tags: false
+            },
+            {
+              name: 'Skill name 11',
+              avatar: 'offline_bolt',
+              tags: false
+            },
+            {
+              name: 'Skill name 12',
+              avatar: 'offline_bolt',
+              tags: false
+            },
+            {
+              name: 'Skill name 13',
+              avatar: 'offline_bolt',
+              tags: false
+            },
+            {
+              name: 'Skill name 14',
+              avatar: 'offline_bolt',
+              tags: false
+            }
+          ]
+        }
+      },
+      methods: {
+        showDialogOfTags: function (tags) {
+          if (tags === 'consistTags') {
+            this.consistTags = true
+          } else {
+            this.consistTags = false
+          }
+          this.dialogAddTag = true
         }
       }
     }
